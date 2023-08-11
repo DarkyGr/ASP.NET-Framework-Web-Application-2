ecc1b3fab6b46f2c1da3ed5874c2947ba6718084
Guillermo Riaño <guilledark17@gmail.com>
2023-08-11 12:25:24 -0600

    🍱 Resources

---
+++ APS.NET_Proyecto_GRM/APS.NET_Proyecto_GRM/Utilidades/Util.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace APS.NET_Proyecto_GRM.Utilidades
+{
+    public class Util
+    {
+        public static void SweetBox(string ex, string sub, string type, Page pg, object obj)
+        {
+            string s = "<SCRIPT languaje='javascript'>swal('" + ex.Replace("\r\n", "\\n").Replace("'", "") + "','" + sub.Replace("\r\n", "\\n").Replace("'", "") + "','" + type + "')</SCRIPT";
+            Type csType = obj.GetType();
+            ClientScriptManager cs = pg.ClientScript;
+            cs.RegisterClientScriptBlock(csType, s, s.ToString());
+        }
+    }
+}
